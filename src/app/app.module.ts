281ce3c322f3954ee56ae9d40eaf4225412df484
Fernando Salazar <FernandoJSR5>
2019-12-30 00:00:38 -0300

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service'
+import { DataStorageService } from './shared/data-storage.service';
 
 @NgModule({
   declarations: [
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,16 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, tap } from "rxjs/operators";
 
-import { RecipeService } from '../recipes/recipe.service'
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-    storeRecipes() {
-        const recipes = this.recipeService.getRecipes();
-        this.http.put('https://ng-course-recipe-book-dbe15.firebaseio.com/recipes.json', recipes).subscribe(response => {
-            console.log(response);
-        });
-    }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        "https://ng-course-recipe-book-dbe15.firebaseio.com/recipes.json",
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        "https://ng-course-recipe-book-dbe15.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
 }
